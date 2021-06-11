67d1fc6a266aefa2cc735e38d688886f8ceab651
eduard.danila <eduard.danila@electrolux.com>
2021-06-11 14:28:14 +0300

    S

---
+++ S/good.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+class Logger {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(text) {
+
+        this.entries.push(text);
+        return ++Logger.count;
+    }
+
+    removeEntry(index) {
+        delete this.entries[index];
+    }
+
+    toString() {
+        return Object.values(this.entries).join('\n');
+    }
+}
+
+
+class PersistenceManager {
+    saveToFile(logger, filename) {
+        fs.writeFileSync(filename, logger.toString());
+    }
+}
+
+let logger = new Logger();
+logger.addEntry('Request received');
+logger.addEntry('Request is in progress');
+logger.addEntry('Request is done');
+console.log(logger.toString());
+
+let p = new PersistenceManager();
+let filename = 'c:/temp/logs.txt';
+p.saveToFile(logger, filename);
+
+// separation of concerns
